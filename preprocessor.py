1f76b54671d417794dd97095609e9dab29fc768f
jordanpottruff <jordanpottruff@gmail.com>
2019-09-04 18:59:07 -0600

    Add function for opening breast cancer data.

--- preprocessor.py
+++ preprocessor.py
@@ -1,5 +1,10 @@
-
 import csv
+
+BREAST_CANCER_DATA_FILE_NAME = "data/breast-cancer-wisconsin.data"
+GLASS_DATA_FILE_NAME = "data/glass.data"
+HOUSE_VOTES_DATA_FILE_NAME = "data/house-votes-84.data"
+IRIS_DATA_FILE_NAME = "data/iris.data"
+SOYBEAN_SMALL_DATA_NAME = "data/soybean-small.data"
 
 # Returns the data from the file as a 2D list (table).
 def get_original_data(filename):
@@ -23,9 +28,11 @@ def remove_missing_rows(data):
             newData.append(line)
     return newData
 
-# Prints a 2d list of data
 def print_data(data):
     for line in data:
         print(line)
 
+# Returns the 2D list of the breast cancer data.
+def open_breast_cancer_data():
+    return get_original_data(BREAST_CANCER_DATA_FILE_NAME)
 
