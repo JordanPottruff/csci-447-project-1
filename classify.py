5b6adfc03c906423b57bc0fddca339c2d9051364
jordanpottruff <jordanpottruff@gmail.com>
2019-09-09 18:20:44 -0600

    Fix house data classification.

--- classify.py
+++ classify.py
@@ -1,6 +1,10 @@
 import naive_bayes as nb
 
 
+# Returns the results of classifying the test data using the specified training data. The result is a list of tuples
+# that represents the results for each row in the test data. The first item in the tuple is the actual class for the
+# observation, and the second item in the tuple is a map of each class to the calculated probability that the
+# observation belongs to the class.
 def classify_house_data(training, test):
     classes = ['republican', 'democrat']
     attribute_cols = list(range(1, 17))
@@ -8,8 +12,11 @@ def classify_house_data(training, test):
 
     results = []
     for line in test:
-        result = {}
+        probabilities = {}
         for cls in classes:
-            result[cls] = nb.calc_C(training, line, attribute_cols, class_col, cls)
+            probabilities[cls] = nb.calc_C(training, line, attribute_cols, class_col, cls)
+
+        results.append((line[class_col], probabilities))
 
     return results
+
