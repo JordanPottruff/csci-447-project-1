59a2656976d486b99765c5f47613199caf9b15cf
Jacob Weikert <Weikjaco@gmail.com>
2019-09-10 10:56:18 -0600

    Update LossFunctions.py

--- LossFunctions.py
+++ LossFunctions.py
@@ -1,10 +1,12 @@
 import math as m
 import numpy as np
 
-def cross_entropy_loss(element): #Taking in arrays of probabilities for each element
+
+def cross_entropy_loss(element):  # Taking in arrays of probabilities for each element
     """Loss Function algorithm that determines our algorithms performs on the data-set provided."""
+
     # We are taking in the truth and predicted values for each element in our test set and going to calculate our loss.
-    
+
     def calculate_loss(truth, predicted):
         return (-1) * (truth * m.log(predicted))
 
@@ -16,20 +18,37 @@ def cross_entropy_loss(element): #Taking in arrays of probabilities for each ele
     # Sum over binary cross-entropies for individual points in a dataset
     num_of_elements = len(truth)  # Number of elements in our test set
     total_element_loss = 0
-    for i in range(num_of_elements-1):
+    for i in range(num_of_elements - 1):
         total_element_loss += calculate_loss(truth[i], predicted[i])
 
     return performance
 
 
-def mean_square_error(element):
+def mean_square_error(example_data):
     # Num of classes is the number of keys -- Jordan will pass predicted value in as a map
     # Mean Square Error (MSE) loss function that will 
-    
-    # predicted = (Actual class, {Key=Classname : Value=Probability, Classname: Value})
+
+    for eachExample in example_data:
+        extractData(eachExample);
+
+
+
+
+
+    # predicted = (Actual class, {Key=Classname : Value=(Probability, Classname: Value)})
     num_of_elements = len(truth)  # Number of elements in our test set
     total_element_loss = 0
-    for i in range(num_of_elements-1):
-        total_element_loss += (predicted-true)**2  # Calculate the squared error for all the instances
-    mean_element_loss = total_element_loss/num_of_elements  # Get average loss for each instance
+    for i in range(num_of_elements - 1):
+        total_element_loss += (predicted - true) ** 2  # Calculate the squared error for all the instances
+    mean_element_loss = total_element_loss / num_of_elements  # Get average loss for each instance
     return mean_element_loss
+
+def extractData(eachExample):
+    """Extracts the truth value, the estimated probability value, the classname, and the number of classes"""
+    for truth, probabilities in eachExample:
+        current_highest_probability = 0
+        for individual_probability in probabilities:
+            # We are just going to ignore if they are the same probability
+
+            if individual_probability > current_highest_probability:
+                current_highest_probaility = individual_probability
